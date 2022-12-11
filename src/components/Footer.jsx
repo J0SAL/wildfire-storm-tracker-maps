15c88608d751657136b11e24511d669e0d6319f6
Josal <joyalmeida321@gmail.com>
2022-12-11 14:02:44 +0530

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
     return (
         <div className='footer'>
-            <a href='https://github.com/J0sal'>By @Joy Almeida</a>
+            <a href='https://github.com/J0sal' target="_blank" >By @Joy Almeida</a>
         </div>
     )
 }
