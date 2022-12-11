a6da47622b23431e841f5e5119ab3d0be80bbd45
Josal <joyalmeida321@gmail.com>
2022-12-11 13:26:21 +0530

    swapped google maps to mapbox

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
-import { useState, useEffect } from 'react'
-import Map from './components/Map'
-import Loader from './components/Loader'
-import Header from './components/Header'
+import { useState, useEffect } from "react";
+import MapView from "./components/Map";
+import Loader from "./components/Loader";
+import Header from "./components/Header";
+import MapBox from "./components/MapBox";
 
 function App() {
-  const [eventData, setEventData] = useState([])
-  const [loading, setLoading] = useState(false)
+  const [eventData, setEventData] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const fetchEvents = async () => {
-      setLoading(true)
-      const res = await fetch(process.env.REACT_APP_NASA_URL)
-      const { events } = await res.json()
+      setLoading(true);
+      const res = await fetch(process.env.REACT_APP_NASA_URL);
+      const { events } = await res.json();
 
-      setEventData(events)
-      setLoading(false)
-    }
+      setEventData(events);
+      setLoading(false);
+    };
 
-    fetchEvents()
-  }, [])
+    fetchEvents();
+  }, []);
 
   return (
     <div>
       <Header />
-      {!loading ? <Map eventData={eventData} /> : <Loader />}
+      {/* {!loading ? <MapView eventData={eventData} /> : <Loader />} */}
+      <MapBox eventData={eventData} />
     </div>
   );
 }
